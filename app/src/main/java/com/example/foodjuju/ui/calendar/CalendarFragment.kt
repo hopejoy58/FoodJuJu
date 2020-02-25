1d264c15a06ff53aaef4a30c9a7b76a8fff927ef
Hope Wilkinson <wilkinhj@mail.uc.edu>
2020-02-25 18:10:36 -0500

    Renamed files

--- app/src/main/java/com/example/foodjuju/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/foodjuju/ui/calendar/CalendarFragment.kt
@@ -1,4 +1,4 @@
-package com.example.foodjuju.ui.notifications
+package com.example.foodjuju.ui.calendar
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.foodjuju.R
 
-class NotificationsFragment : Fragment() {
+class CalendarFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var calendarViewModel: CalendarViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(this, Observer {
+        calendarViewModel =
+                ViewModelProviders.of(this).get(CalendarViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        calendarViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
