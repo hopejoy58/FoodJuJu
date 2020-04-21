73e19471a5dfea33bfbe52c06192d2585b28fe82
hopejoy58 <wilkinhj@mail.uc.edu>
2020-04-21 19:43:10 -0400

    Added food and mood icons and a recyclerView on the homepage

--- app/src/main/java/com/example/foodjuju/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/foodjuju/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.example.foodjuju.R
 
 class HomeFragment : Fragment() {
@@ -22,10 +23,13 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val recycleView: RecyclerView? = root.findViewById(R.id.view_recycler)
         return root
     }
 }
