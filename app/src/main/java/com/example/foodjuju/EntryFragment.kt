5573650c20b67aa0f2f18b34f247ba789be15473
hopejoy58 <wilkinhj@mail.uc.edu>
2020-04-23 19:21:16 -0400

    These were supposed to be included in last commit

--- app/src/main/java/com/example/foodjuju/EntryFragment.kt
+++ app/src/main/java/com/example/foodjuju/EntryFragment.kt
@@ -1,4 +1,4 @@
-package com.example.foodjuju.entry
+package com.example.foodjuju
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
-import com.example.foodjuju.R
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
