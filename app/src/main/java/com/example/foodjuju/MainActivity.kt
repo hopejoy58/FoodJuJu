a2e7698ba5429c0877fe4a703c28e8486106b7fd
hopejoy58 <wilkinhj@mail.uc.edu>
2020-04-23 18:09:45 -0400

    Converting to two-pageapp

--- app/src/main/java/com/example/foodjuju/MainActivity.kt
+++ app/src/main/java/com/example/foodjuju/MainActivity.kt
@@ -1,113 +1,27 @@
 package com.example.foodjuju
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.ktx.Firebase
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var foodName: EditText
-    lateinit var foodDescription: EditText
-    lateinit var foodIngredients: EditText
-    lateinit var mood: EditText
-    lateinit var moodComments: EditText
-   // lateinit var btn_cancel: Button
-   // lateinit var btn_save: Button
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_calendar, R.id.navigation_home, R.id.navigation_report))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-        val fab: View = findViewById(R.id.fab_newEntry)
-        fab.setOnClickListener {
-
-            setContentView(R.layout.fragment_entry)
-
-            saveData()
-/*
-            Toast.makeText(
-                MainActivity.this,
-                "Firebase connection success.",
-                Toast.LENGTH_LONG
-            ).show();
-
-*/
-
-/*
-            btn_cancel.setOnClickListener {
-
-            }
-         btn_save.setOnClickListener{
-                saveData()
-            }
-*/
-
-        }
-
-
+        setSupportActionBar(toolbar)
     }
 
-    private fun saveData(){
-
-        foodName = findViewById(R.id.foodName)
-        foodDescription = findViewById(R.id.foodDescription)
-        foodIngredients = findViewById(R.id.foodIngredients)
-        mood = findViewById(R.id.mood)
-        moodComments = findViewById(R.id.moodComments)
-
-        val food = foodName.text.toString().trim()
-        val description = foodDescription.text.toString().trim()
-        val ingredients = foodIngredients.text.toString().trim()
-        val foodMood = mood.text.toString().trim()
-        val comments = moodComments.text.toString().trim()
-
-        // check if fields are empty
-        if (food.isEmpty()){
-            foodName.error = "Please enter name of food."
-            return
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
         }
-        if (description.isEmpty()){
-            foodDescription.error = "Please enter description of food."
-            return
-        }
-        if (ingredients.isEmpty()){
-            foodIngredients.error = "Please enter ingredients of food."
-            return
-        }
-        if (foodMood.isEmpty()){
-            mood.error = "Please enter mood."
-            return
-        }
-
-
-        val ref = FirebaseDatabase.getInstance().getReference("foods")
-        val foodId = ref.push().key.toString()
-        val foods = foodData(foodId, food, description, ingredients, foodMood, comments)
-
-        ref.child(foodId).setValue(foods).addOnCompleteListener {
-            Toast.makeText(applicationContext, "Successfully saved to database.", Toast.LENGTH_LONG)
-                .show()
-        }
-
     }
 }
--- app/src/main/java/com/example/foodjuju/foodData.kt
+++
@@ -1,3 +0,0 @@
-package com.example.foodjuju
-
-class foodData(val id: String, val food: String, val foodType: String, val ingredients: String, val mood: String, val moodComment: String)
